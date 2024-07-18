39c6a744b49bcd7918c4d4500003984c0b7a1ae1
Anandhan Sreekumar <anandhan.sreekumar@vonnue.com>
2024-07-18 17:13:39 +0530

    feat: query and query all by

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Props } from "./skills.types";
 
 const Skills: React.FC<Props> = ({ skills }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
-    <ul>
-      {skills.map((skill) => (
-        <li key={skill}>{skill}</li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {skills.map((skill) => (
+          <li key={skill}>{skill}</li>
+        ))}
+      </ul>
+      {isLoggedIn ? (
+        <button>Start learning</button>
+      ) : (
+        <button onClick={handleLogin}>Login</button>
+      )}
+    </>
   );
 };
 
--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -18,4 +18,20 @@ describe("Skills", () => {
     const listElements = screen.getAllByRole("listitem");
     expect(listElements).toHaveLength(skills.length);
   });
+
+  test("renders login button", () => {
+    render(<Skills skills={skills} />);
+
+    const loginButton = screen.getByRole("button", { name: "Login" });
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  test("start learning button is not rendered", () => {
+    render(<Skills skills={skills} />);
+
+    const startLearningButton = screen.queryByRole("button", {
+      name: "Start learning",
+    });
+    expect(startLearningButton).not.toBeInTheDocument();
+  });
 });
